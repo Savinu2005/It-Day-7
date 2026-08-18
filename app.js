d2ebe1126c6e6a04dae4cec8f199a64a6519876c
Savinu2005 <palansooriyasavinu@gmail.com>
2026-08-18 21:00:26 +0530

    Update Swal.fire configuration in btnGuessNumberOnAction function

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ console.log(randomNum);
 
 function btnGuessNumberOnAction() {
     Swal.fire({
-  title: "Good job!",
-  text: "You clicked the button!",
-  icon: "success"
-});
+        title: "Custom width, padding, color, background.",
+        width: 600,
+        padding: "3em",
+        color: "#716add",
+        background: "#fff url(/images/trees.png)",
+        backdrop: `
+    rgba(0,0,123,0.4)
+    url("/images/nyan-cat.gif")
+    left top
+    no-repeat
+  `
+    });
 }

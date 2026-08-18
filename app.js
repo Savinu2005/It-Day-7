04cde653d6a5c88ebfd2a7bae72b46fff80ab5ff
Savinu2005 <palansooriyasavinu@gmail.com>
2026-08-18 21:52:48 +0530

    Comment out unused code and disable Swal.fire script import

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
-let num = Math.random() * 10 + 1;
-let randomNum = Math.floor(num);
+// let num = Math.random() * 10 + 1;
+// let randomNum = Math.floor(num);
 
-console.log(randomNum);
+// console.log(randomNum);
 
-function btnGuessNumberOnAction() {
-    Swal.fire({
-        title: "Custom width, padding, color, background.",
-        width: 600,
-        padding: "3em",
-        color: "#716add",
-        background: "#fff url(/images/trees.png)",
-        backdrop: `
-    rgba(0,0,123,0.4)
-    url("/images/nyan-cat.gif")
-    left top
-    no-repeat
-  `
-    });
-}
+// function btnGuessNumberOnAction() {
+//     Swal.fire({
+//         title: "Custom width, padding, color, background.",
+//         width: 600,
+//         padding: "3em",
+//         color: "#716add",
+//         background: "#fff url(/images/trees.png)",
+//         backdrop: `
+//     rgba(0,0,123,0.4)
+//     url("/images/nyan-cat.gif")
+//     left top
+//     no-repeat
+//   `
+//     });
+// }
+
+
+
+
+
+// let
+// var
+// const
+
+
+
+
+// {
+//     let name = "John Doe";
+//     var age = 30;
+// }
+
+// console.log(name);
+// console.log(age);
+
+
+// const customerList = [];
+
+// customerList.push("John Doe");
+
+// console.log(customerList);
+
+// // customerList = "saman"
+
+// console.log(typeof customerList);
+
+
+

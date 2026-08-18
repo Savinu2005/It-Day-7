8f556684465ad3fbf4508da6db02a4e090b0932f
Savinu2005 <palansooriyasavinu@gmail.com>
2026-08-18 21:58:36 +0530

    Refactor customerList initialization and add reverse functionality

--- app.js
+++ app.js
@@ -50,4 +50,13 @@
 // console.log(typeof customerList);
 
 
+const customerList = [];
 
+
+customerList.push(1);
+customerList.push(2);
+customerList.push(3);
+
+let revAr = customerList.reverse();
+
+console.log(revAr);

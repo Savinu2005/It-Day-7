f7bff08e47e88dd4229d0acfe1d6b7237438db9e
Savinu2005 <palansooriyasavinu@gmail.com>
2026-08-19 23:25:39 +0530

    Add product fetching and display functionality with Bootstrap styling

--- app.js
+++ app.js
@@ -193,3 +193,36 @@
 //     tblTodoList.innerHTML= body;
 
 // })
+
+
+fetch("https://fakestoreapi.com/products/1").then(res=>res.json()).then(data=>{
+    console.log(data);
+
+    let productContainer = document.getElementById("productContainer");
+    let body = "";
+    data.forEach(element => {
+        body+= `<div class="col">
+                    <div class="card shadow-sm">
+                        <svg aria-label="Placeholder" class="bd-placeholder-img card-img-top" height="225" preserveAspectRatio="xMidYMid slice" role="img" width="100%" xmlns="http://www.w3.org/2000/svg">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                            <p class="card-text">${element.title}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-body-secondary">$${element.price.toFixed(2)}</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+    });
+
+productContainer.innerHTML = body;
+
+
+})
+    

d0cd74bad08d8f998b0d6ba4132442beb8ff8580
Savinu2005 <palansooriyasavinu@gmail.com>
2026-08-19 22:57:54 +0530

    Comment out fetch example for todo list and add Bootstrap styling to the HTML structure

--- app.js
+++ app.js
@@ -172,24 +172,24 @@
 
 
 
-fetch("https://jsonplaceholder.typicode.com/todos").then(res=>res.json()).then(data=>{
-    console.log(data);
+// fetch("https://jsonplaceholder.typicode.com/todos").then(res=>res.json()).then(data=>{
+//     console.log(data);
 
-    let tblTodoList = document.getElementById("tblTodoList");
+//     let tblTodoList = document.getElementById("tblTodoList");
 
-    let body=""
+//     let body=""
 
-    data.forEach(element => {
-        body+= `
-                <tr>
-            <td>${element.id}</td>
-            <td>${element.title}</td>
-            <td>${element.userId}</td>
-            <td>${element.completed}</td>
-        </tr>
-        `
-    });
+//     data.forEach(element => {
+//         body+= `
+//                 <tr>
+//             <td>${element.id}</td>
+//             <td>${element.title}</td>
+//             <td>${element.userId}</td>
+//             <td>${element.completed}</td>
+//         </tr>
+//         `
+//     });
 
-    tblTodoList.innerHTML= body;
+//     tblTodoList.innerHTML= body;
 
-})
+// })
